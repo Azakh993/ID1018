553c211843f3d7937c88abefe1164945c9026845
Azmeer K <59706003+Azakh993@users.noreply.github.com>
2022-02-08 14:59:33 +0100

    Update Triangle.java

--- OU2/Triangle.java
+++ OU2/Triangle.java
@@ -1,14 +1,12 @@
 
 class Triangle
 {   
-    // Calculates a triangles area using base and height
     public static double areaBaseHeight(double a, double b)
     {
         double area = a * b / 2;
         return area;
     }
     
-    // Calculates a triangles area using two sides and the angle inbetween
     public static double areaAngleSides(double ab, double a, double b)
     {
         double d = (ab * Math.PI) / 180;
@@ -16,21 +14,18 @@ class Triangle
         return area;
     }
 
-    // Calculates a triangles perimeter using all three sides
     public static double perimeterSides(double a, double b, double c)
     {
         double perimeter = a + b + c;
         return perimeter;
     }
 
-    // Calculates a triangles hypothenuse using the two shorter sides
     public static double hypothenuseSides(double a, double b)
     {
         double hypothenuse = Math.sqrt(a * a + b * b);
         return hypothenuse;
     }
 
-    // Calculates one of a triangles bisectors using two sides and the angle inbetween
     public static double bisectorAngleSides(double ab, double a, double b)
     {
         double d = (ab * Math.PI) / 180;
@@ -38,7 +33,6 @@ class Triangle
         return bisector;
     }
 
-    // Calculates an inradius using all three sides of a triangle
     public static double inradiusSides(double a, double b, double c)
     {
         double s = (a + b + c) / 2;
@@ -46,7 +40,6 @@ class Triangle
         return inradius;
     }
 
-    // Calculates a circumradius using all three sides of a triangle
     public static double circumradiusSides(double a, double b, double c)
     {
         double s = (a + b + c) / 2;
